c3acc6513d8f581a9d0905a334c231ab0ee32ce1
Matt Williams <matt.seongjun@gmail.com>
2019-04-03 21:01:54 -0400

    Please insert a nickel

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,51 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+    puts "I accept the following commands:"
+    puts "- help : displays this help message"
+    puts "- list : displays a list of songs you can play"
+    puts "- play : lets you choose a song to play"
+    puts "- exit : exits this program"
+end
+
+def play(songs) 
+    puts "Please enter a song name or number:"
+    input = gets.chomp
+    case 
+        when  0 < input.to_i && input.to_i <= songs.length
+            puts "Playing #{songs[input.to_i - 1]}"
+        when songs.include?(input)
+            puts "Playing #{input}"
+        else
+            puts "Invalid input, please try again"
+        end
+end
+
+def list(songs)
+    songs.each_with_index do |song, n|
+        puts "#{n + 1}. #{song}"
+    end
+end
+
+def exit_jukebox
+    puts "Goodbye"
+end
+
+def run(songs)
+    puts "Please enter a command:"
+    cmd = gets.strip
+    case cmd
+        when "help"
+            help()
+        when "list"
+            list(songs)
+        when "play"
+            play(songs)
+        when "exit"
+            exit_jukebox()
+        else
+            puts "Invalid command." 
+            run()
+        end
+end
